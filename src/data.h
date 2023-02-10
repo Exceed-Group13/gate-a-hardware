08657c3f41bf783f55e84e0c296168efa5fc1cc8
Danita Frikaow <92835393+dzptahh@users.noreply.github.com>
2023-02-10 17:39:33 +0700

    Update data.h

--- src/data.h
+++ src/data.h
@@ -3,12 +3,27 @@
 #include <HTTPClient.h>
 
 // declare variable
+const char *ssid = "POCOPHONE";
+const char *password = "a1b2c3d4";
+void Connect_Wifi()
+{
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.print("OK! IP=");
+    Serial.println(WiFi.localIP());
+}
 
+///////////////////////////////////////////////////////////////
 const String baseUrl =
     "https://ecourse.cpe.ku.ac.th/exceed13";
 String result, house_name;
 JsonArray result_array, pin, new_pin;
-bool state;
+bool is_open, alert;
 int delay_val;
 
 void GET_home()
@@ -25,12 +40,13 @@ void GET_home()
         Serial.println(payload);
         deserializeJson(doc, payload);
 
-        result_array = doc["result"].as<JsonArray>();
+        result_array = doc["result"].as<JsonArray>(); // set array
 
-        state = result_array[0]["state"].as<bool>();
+        is_open = result_array[0]["state"].as<bool>();
         house_name = result_array[0]["house_name"].as<String>();
         delay_val = result_array[0]["delay"].as<int>();
         pin = result_array[0]["pin"].as<JsonArray>();
+        alert = result_array[0]["alert"].as<bool>();
     }
     else
     {
@@ -49,7 +65,7 @@ void PUT_home()
     http.addHeader("Content-Type", "application/json");
 
     DynamicJsonDocument doc(2048);
-    doc["state"] = state;
+    doc["state"] = is_open;
     doc["house_name"] = house_name;
     serializeJson(doc, json);
     Serial.println(json);
@@ -60,35 +76,6 @@ void PUT_home()
         Serial.print("Done");
         Serial.println();
     }
-    else
-    {
-        Serial.print("Error ");
-        Serial.println(httpResponseCode);
-    }
-
-    Serial.println("----------------------------------");
-}
-
-void GET_resetpin()
-{
-    DynamicJsonDocument doc(4096);
-    HTTPClient http;
-    const String url = baseUrl + "/resetpin";
-    http.begin(url);
-
-    int httpResponseCode = http.GET();
-    if (httpResponseCode == 200)
-    {
-        String payload = http.getString();
-        Serial.println(payload);
-        deserializeJson(doc, payload);
-
-        result_array = doc["result"].as<JsonArray>();
-
-        house_name = result_array[0]["house_name"].as<String>();
-        pin = result_array[0]["pin"].as<JsonArray>();
-    }
-
     else
     {
         Serial.print("Error ");
