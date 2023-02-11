43f4c5d71ca4255a3cbf88dd3e9bd2b02e22cc4c
PanyawatKRONGKITICHU <89672817+KrongPan@users.noreply.github.com>
2023-02-11 10:49:34 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -28,34 +28,36 @@ Bounce debouncer3 = Bounce();
 Bounce debouncer4 = Bounce();
 Bounce debouncer5 = Bounce();
 // global var
-//pin
+// pin
 int key[3];
-//int pin[3] = {1, 2, 3};
+// int pin[3] = {1, 2, 3};
 bool is_pass;
 int index_pin = 0;
 int count = 0;
 int demo_count = 3;
-//servo
+// servo
 Servo myservo;
 bool goto_door = false;
-//is_open = false;
+// is_open = false;
 int door_position = 45;
-//buzzer
+// buzzer
 int buzzerPin = 4;
 bool is_state_change = false;
-//Avoidance
+// Avoidance
 int laser = 22;
 int threshold = 3500;
 bool is_ob = false;
-//timer
+unsigned long time_now = 0;
+// timer
 bool is_time = 0;
 int count_time = 0;
 int demo_timer = 5;
-//
+// data
 bool is_post_data = false;
 bool is_get_data = false;
-
+// task
 TaskHandle_t TaskGet;
+
 void setup()
 {
   Serial.begin(115200);
@@ -75,75 +77,78 @@ void setup()
   debouncer3.interval(25);
   debouncer4.interval(25);
   debouncer5.interval(25);
-  //servo
+  // servo
   myservo.attach(13);
   myservo.write(45);
-  //buzzer
-  pinMode (buzzerPin, OUTPUT);
-  //Avoidance
+  // buzzer
+  pinMode(buzzerPin, OUTPUT);
+  // Avoidance
   pinMode(laser, OUTPUT);
   Connect_Wifi();
   GET_home();
   // mutiTask
-  xTaskCreatePinnedToCore(pin_c, "buttons", 10240, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(servo_door_open, "open", 10240, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(servo_door_close, "close", 10240, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(time_count, "timer", 10240, NULL, 2, NULL, 1);
-  //xTaskCreatePinnedToCore(obstacle, "LDR&laser", 1024, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(pin_c, "buttons", 1024, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(servo_door_open, "open", 1024, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(servo_door_close, "close", 1024, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(time_count, "timer", 1024, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(obstacle, "LDR&laser", 2048, NULL, 3, NULL, 1);
   xTaskCreatePinnedToCore(GET_data, "GET_data", 10240, NULL, 2, &TaskGet, 0);
   xTaskCreatePinnedToCore(PUT_data, "PUT_data", 10240, NULL, 1, NULL, 0);
-
 }
 
-void loop(){}
+void loop() {}
 
 void GET_data(void *param)
 {
-  while(true)
+  while (true)
   {
-      GET_home();
-      if(door_position==135 || door_position==45)
-        is_open = temp_is_open;  
-      Serial.println();
-      if(is_open != is_state_change)
-      {
-        goto_door = 1;
-        is_state_change = is_open;
-      }
+    GET_home();
+    if (door_position == 135 || door_position == 45)
+      is_open = temp_is_open;
+    Serial.println();
+    if (is_open != is_state_change)
+    {
+      goto_door = 1;
+      is_state_change = is_open;
     }
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
 
 void PUT_data(void *param)
 {
-  while(true)
+  while (true)
   {
-    if(is_post_data)
+    if (is_post_data)
     {
       Serial.println("PUT");
       vTaskSuspend(TaskGet);
       PUT_home();
       is_post_data = 0;
+      time_now = millis();
+      while(millis() < time_now + 1000){
+      //wait approx. [period] ms
+      }
       vTaskResume(TaskGet);
     }
-    vTaskDelay(500/portTICK_PERIOD_MS);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
 void time_count(void *param)
 {
-  while(true)
+  while (true)
   {
-    if(is_time)
+    if (is_time)
     {
-      if(!is_open)
+      if (!is_open)
       {
         is_time = 0;
         count_time = 0;
       }
       Serial.println(count_time);
       count_time++;
-      if(count_time == demo_timer+1)
+      if (count_time == delay_val + 1)
       {
         is_time = 0;
         goto_door = 1;
@@ -152,70 +157,74 @@ void time_count(void *param)
         is_post_data = 1;
       }
     }
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
 void obstacle(void *param)
 {
-  while(true)
+  while (true)
   {
     if (is_ob)
     {
       digitalWrite(laser, HIGH);
-      delay(10);
+      delay(100);
       int light_ldr = analogRead(LDR);
       Serial.println(light_ldr);
-      while(light_ldr < threshold)
+      while (light_ldr < threshold)
       {
-        int light_ldr = analogRead(LDR);
-        delay(10);
+        Serial.println("OBSTACLE");
+        light_ldr = analogRead(LDR);
+        time_now = millis();
+        while(millis() < time_now + 1000){
+        //wait approx. [period] ms
+        }
       }
       is_ob = 0;
       digitalWrite(laser, LOW);
     }
-    vTaskDelay(50/portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
   }
 }
 
 void servo_door_open(void *param)
 {
-
   while (true)
   {
-    if(goto_door)
+    if (goto_door)
     {
-      if(is_open)
+      if (is_open)
       {
         Serial.println("iam open");
-        if(door_position < 135)
+        if (door_position < 135)
         {
           myservo.write(door_position);
           door_position++;
         }
-        else if(door_position == 135)
+        else if (door_position == 135)
         {
           goto_door = 0;
           is_time = 1;
         }
-      } 
+      }
     }
-    vTaskDelay(30/portTICK_PERIOD_MS);
+    vTaskDelay(30 / portTICK_PERIOD_MS);
   }
 }
 
 void servo_door_close(void *param)
 {
-  while(true)
+  while (true)
   {
-    if(goto_door)
+    if (goto_door)
     {
-      if(!is_open)
+      if (!is_open)
       {
         Serial.println("iam close");
         is_ob = 1;
-        if(door_position > 45)
+        if (door_position > 45)
         {
+          is_ob = 1;
           myservo.write(door_position);
           door_position--;
         }
@@ -225,7 +234,7 @@ void servo_door_close(void *param)
         }
       }
     }
-    vTaskDelay(50/portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
   }
 }
 
@@ -255,12 +264,12 @@ void pin_c(void *param)
     }
     else if (debouncer4.fell())
     {
-      if(index_pin > 0)
+      if (index_pin > 0)
       {
         key[index_pin] = 0;
         index_pin--;
       }
-      else if(index_pin == 0)
+      else if (index_pin == 0)
       {
         key[index_pin] = 0;
       }
@@ -272,32 +281,32 @@ void pin_c(void *param)
       is_open = !is_open;
       is_post_data = 1;
     }
-    //เเสดงไฟ
-    if(index_pin == 0) 
+    // เเสดงไฟ
+    if (index_pin == 0)
     {
       digitalWrite(led1, 0);
       digitalWrite(led2, 0);
       digitalWrite(led3, 0);
     }
-    else if(index_pin == 1)
+    else if (index_pin == 1)
     {
       digitalWrite(led1, 1);
       digitalWrite(led2, 0);
       digitalWrite(led3, 0);
     }
-    else if(index_pin == 2)
+    else if (index_pin == 2)
     {
       digitalWrite(led1, 1);
       digitalWrite(led2, 1);
       digitalWrite(led3, 0);
     }
-    else if(index_pin == 3)
+    else if (index_pin == 3)
     {
       digitalWrite(led1, 1);
       digitalWrite(led2, 1);
       digitalWrite(led3, 1);
     }
-    //ตรวจสอบความถูกต้อง
+    // ตรวจสอบความถูกต้อง
     if (index_pin == 3)
     {
       index_pin = 0;
@@ -316,16 +325,15 @@ void pin_c(void *param)
       if (is_pass)
       {
         count = 0;
-        if(!is_open)
+        if (!is_open)
         {
           digitalWrite(led4, 1);
-          digitalWrite (buzzerPin, HIGH);
+          digitalWrite(buzzerPin, HIGH);
           goto_door = 1;
           is_open = 1;
           is_post_data = 1;
-          //post
           delay(500);
-          digitalWrite (buzzerPin, LOW);
+          digitalWrite(buzzerPin, LOW);
           delay(1000);
           digitalWrite(led4, 0);
         }
@@ -336,33 +344,32 @@ void pin_c(void *param)
         }
       }
       else
-      { 
+      {
         count++;
-        for(int i = 0; i < 3; i++)
-          {
-            digitalWrite (buzzerPin, HIGH);
-            delay (50);
-            digitalWrite (buzzerPin, LOW);
-            delay (50);
-          }
-        if(count == 3)
+        for (int i = 0; i < 3; i++)
+        {
+          digitalWrite(buzzerPin, HIGH);
+          delay(50);
+          digitalWrite(buzzerPin, LOW);
+          delay(50);
+        }
+        if (count == 3)
         {
           goto_door = 1;
           is_open = 0;
           digitalWrite(led1, 0);
           digitalWrite(led2, 0);
           digitalWrite(led3, 0);
-          for(int i = 0; i < 100; i++)
+          for (int i = 0; i < 70; i++)
           {
-            digitalWrite (buzzerPin, HIGH);
+            digitalWrite(buzzerPin, HIGH);
             digitalWrite(led4, 1);
-            delay (50);
-            digitalWrite (buzzerPin, LOW);
+            delay(50);
+            digitalWrite(buzzerPin, LOW);
             digitalWrite(led4, 0);
-            delay (50);
+            delay(50);
           }
           count = 0;
-          //post
         }
       }
     }
